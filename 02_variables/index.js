77c84e8bdf2c3ecf997cb47a19ed45edfbbb3508
pardipYadav <213sunilyadav@gmail.com>
2025-04-17 21:46:40 +0530

    day 2 variables

---
+++ 02_variables/index.js
@@ -0,0 +1,22 @@
+/*
+If you want to read more description check this link : https://longing-swamp-5af.notion.site/JavaScript-19ed0bd3751d807f8472e45ae06293a2
+*/
+
+// in javascript, we declare variables using var, let, and const
+
+// var
+// let
+// const
+
+// 1. var - old way function-scoped
+
+var naam = "Pardeep";
+naam = "rahul";
+console.log(naam);
+
+let age = 25;
+age = 355;
+console.log(age);
+
+const country = "India";
+console.log(country); // Output: India
---
+++ Introduction-of-program/index.js
@@ -0,0 +1,4 @@
+/*
+Please click on the link below to full introduction content:
+*/
+"https://www.notion.so/Javascript-19ed0bd3751d807f8472e45ae06293a2?pvs=4";
